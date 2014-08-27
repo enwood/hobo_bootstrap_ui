3635f7cd8e4872d12b8be71a50317abfa2e55f7d
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-08-27 12:05:35 +0200

    modal ported

--- vendor/assets/javascripts/hobo-bootstrap-ui/hjq-modal.js
+++ vendor/assets/javascripts/hobo-bootstrap-ui/hjq-modal.js
@@ -7,7 +7,6 @@
     };
 
     $.fn.hjq_modal = function( method ) {
-
         if ( methods[method] ) {
             return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
         } else if ( typeof method === 'object' || ! method ) {
